8616154570ef50abb18e4ca406c3e4d41be34243
Jan Altensen <info@stricted.net>
2020-05-12 20:14:32 +0200

    s5neolte: move libstagefright_shim to system
     * setting `proprietary: true,` breaks building of treble devices when this tree is synced aswell
    
    Change-Id: I98f490942bd03c9bd453c1bd14dafafda65ab34c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,7 @@ TARGET_KERNEL_CONFIG := lineageos_s5neolte_defconfig
 # Shim
 TARGET_LD_SHIM_LIBS += \
     /system/lib/libcamera_client.so|/vendor/lib/libcamera_client_shim.so \
-    /system/lib/libstagefright.so|/vendor/lib/libstagefright_shim.so \
+    /system/lib/libstagefright.so|/system/lib/libstagefright_shim.so \
     /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so
 
 # Legacy BLOB Support
